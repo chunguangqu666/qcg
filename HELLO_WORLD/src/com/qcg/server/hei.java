b18069c6094002610e6e37f7b01873707abb447e
quchunguang <quchungg@126.com>
2019-11-27 17:08:21 +0800

    aaa

--- HELLO_WORLD/src/com/qcg/server/hei.java
+++ HELLO_WORLD/src/com/qcg/server/hei.java
@@ -4,6 +4,6 @@ public class hei {
     public static void main(String[] args) {
         System.out.println("hahaha");
         System.out.println("hahaha");
-        System.out.println("嘿嘿嘿");
+        System.out.println("嘿嘿嘿aaa");
     }
 }
